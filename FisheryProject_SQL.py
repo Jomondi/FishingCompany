98c48fd90de683a796ba14254296f9c72c3ede5e
Jomondi <ajos06@yahoo.com>
2021-03-17 00:27:39 -0400

    Update FisheryProject_SQL.py

--- FisheryProject_SQL.py
+++ FisheryProject_SQL.py
@@ -1,40 +1,6 @@
-# Create a 4 Tables in a Database of your Choice: Populate the tables with your
-# own makeship csv file and join all the tables and display the details.
-# Let's make this due not later than 03/20/2021.
-#
-# 1) Boat Table
-# bout_id Has to be Unique
-# bout_Name
-# boat_size in tones
-# boat_length
-# station_id
-# boat_capacity in number of people it can carry
-# fishing (Yes/No)
-#
-# 2) Fisher Table
-# fisherman_id Has to be Unique
-# fisher_names
-# boat_id Has to be Unique
-# phone_number
-# email_address
-# Age
-#
-# 3) Owner Table
-# owner_id Has to be Unique
-# owner_name
-# boat_id Has to be Unique
-# phone_number
-# email_address
-#
-# 4) Station
-# station_id
-# station_name
-# Address
-
-
 import mysql.connector, csv
 
-conn = mysql.connector.Connect(host='localhost', user='root', password='FidelliaAdhiambo', database='Fishing_Team')
+conn = mysql.connector.Connect(host='localhost', user='root', password='', database='Fishing_Team')
 cursor = conn.cursor()
 print(f'MySQL database version connected to is {conn.get_server_info()}')
 
@@ -116,8 +82,8 @@ def insert_into_boat():
         print("Values entered into boat table successfully!")
 
 
-# insert_into_boat()
-# conn.commit()
+insert_into_boat()
+conn.commit()
 
 
 def insert_into_fishermen():
@@ -128,8 +94,8 @@ def insert_into_fishermen():
         print("Values entered into fishermen table successfully!")
 
 
-# insert_into_fishermen()
-# conn.commit()
+insert_into_fishermen()
+conn.commit()
 
 
 def insert_into_owners():
@@ -140,8 +106,8 @@ def insert_into_owners():
         print("Values entered into owner table successfully!")
 
 
-# insert_into_owners()
-# conn.commit()
+insert_into_owners()
+conn.commit()
 
 
 def insert_into_station():
@@ -152,8 +118,8 @@ def insert_into_station():
         print("Values entered into station table successfully!")
 
 
-# insert_into_station()
-# conn.commit()
+insert_into_station()
+conn.commit()
 
 
 print("\n============================================ JOIN TABLES ==================================================\n")
